3307878a1b2fab59963fd3132a0c9dc989850c09
Samarthasbhat <90254785+Samarthasbhat@users.noreply.github.com>
2022-07-07 23:48:23 +0530

    simulation and flow chart updated

--- deadlock.c
+++ deadlock.c
@@ -1,3 +1,4 @@
+
 #include<GL/glut.h>
 #include<math.h>
 #include<stdio.h>
@@ -5,7 +6,7 @@
 int up=0,down=0;
 int f=1;
 int cc=600,nn=100,ni=800,da=10,ss=200;
-int m=700,n=220;
+int m=750,n=220;
 int s=300,v=800;
 int spotdisp=1;
 
@@ -36,6 +37,52 @@ void renderBitmapString18(float x,float y,const char *string)
 		//GLUT_BITMAP_9_BY_15
 
 }
+
+
+// * * line function * * for line in flow chart
+void line(int a,int b,int c,int d){
+			glColor3f(1,1,1);
+    	glBegin(GL_LINES);
+    	glVertex2f(a,b);
+    	glVertex2f(c,d);
+    	glEnd();
+			}	
+			
+			
+// * * triangle function * * 
+void  arrow(int ta,int tb,int d)
+{
+	
+		glBegin(GL_POLYGON);
+		glColor3ub(254,228,64);
+		if(d==0)
+		{
+			glVertex2f(ta,tb);
+			glVertex2f(ta-5,tb+7);
+			glVertex2f(ta+5,tb+7);
+		}
+		else if(d==1)
+		{
+			glVertex2f(ta,tb);
+			glVertex2f(ta+5,tb+7);
+			glVertex2f(ta+5,tb-7);
+		}
+		else if(d==2)
+		{
+			glVertex2f(ta,tb);
+			glVertex2f(ta-5,tb+7);
+			glVertex2f(ta-5,tb-7);
+		}
+		else if(d==3)
+		{
+			glVertex2f(ta,tb);
+			glVertex2f(ta-7,tb+5);
+			glVertex2f(ta-7,tb+5);
+		}
+		glEnd();
+}
+			
+			
 void myinit()
 {
     	glClearColor(0,0,0,1.0);
@@ -44,13 +91,13 @@ void myinit()
     	glMatrixMode(GL_MODELVIEW);
 }
 
-void flowrect(float x,float y){
+void flowrect(int x,int y,int xs,int ys){
 		glBegin(GL_QUADS);
-    	glColor3f(1,1,0);
+    	glColor3ub(45,20,44);
    	 glVertex2f(x,y);
-   	 glVertex2f(x+70,y);
-   	 glVertex2f(x+70,y-40);
-   	 glVertex2f(x,y-40);
+   	 glVertex2f(x+xs,y);
+   	 glVertex2f(x+xs,y-ys);
+   	 glVertex2f(x,y-ys);
    	 glEnd();
 }
 void SpecialKey(int key,int x,int y){
@@ -330,9 +377,11 @@ void keyboard( unsigned char key, int x, int y )
 
 }
 
-void delay(){
-int i=3900010;
-while(i--);
+void delay(int n){
+int i;
+for(i=2000000;i<n;i--){
+n++;
+}
 }
 
 
@@ -340,7 +389,7 @@ void scene2(){
 	
 	
 	
-	glColor3f(0,1,0);
+	glColor3f(0.05,0.36,0.25);
 	glBegin(GL_POLYGON);
 	glVertex2i(0,0);
 	glVertex2i(700,0);
@@ -348,17 +397,18 @@ void scene2(){
 	glVertex2i(320,800);
 	glVertex2i(0,800);
 	glEnd();
-	
-	glColor3f(0.1,0,0.1);
-	renderBitmapString(200,150,"Press B return to home page");
-	renderBitmapString(200,250,"Press I to Introduction");
-	renderBitmapString(200,350,"Press D to simulation");
-	renderBitmapString(200,450,"Press E to EXIT");
-	renderBitmapString(200,550,"Press H to HELP KEY");
+	glColor3f(1,1,0);
+	renderBitmapString(100,650,"PERSONS : PROCESSES ");
+	renderBitmapString(100,620,"STEPS      : RESOURCES ");
+	glColor3f(1,1,1);
+	renderBitmapString(120,150,"Press B return to home page");
+	renderBitmapString(120,250,"Press I to Introduction");
+	renderBitmapString(120,350,"Press E to EXIT");
+	renderBitmapString(120,450,"Press H to HELP KEY");
 	//steps******************************
 	
 	 glColor3f(0.5, 1,0.5);
-   	 glLineWidth(3);
+   	 glLineWidth(5);
    	 glBegin(GL_POLYGON);
    	 glVertex2i(700, 160);
    	 glVertex2i(650, 250);
@@ -420,14 +470,25 @@ void next()
 	glColor3f(1,1,1);
 	scene2();
 	
-		if(v>500 && n>450){
+		if(v>450 && n>350){
 	
 	glColor3f(1,0,0);
-	renderBitmapString(900,900,"Intruption");
+	renderBitmapString(900,900,"Interruption");
 	
-	delay();
-	glColor3f(0,0,1);
-	renderBitmapString(900,800,"Deadlock occured");
+	delay(50);
+	
+	
+	}
+	if(v>500 && n>420){
+	glLineWidth(8);
+		glPushMatrix(); 
+		char strp[]="DEADLOCK OCCURED";
+		glColor3f(0.9,0.05,0.2);
+		glTranslatef(330,500.0,0.0);
+		glScaled(0.26,0.6,0.0);
+		for(int i=0;strp[i]!='\0';i++)
+		glutStrokeCharacter(GLUT_STROKE_MONO_ROMAN,strp[i]);
+		glPopMatrix();
 	}
 	glPushMatrix();
 	
@@ -436,12 +497,12 @@ void next()
 	if(up==1 && s<500)
 	{
 	
-	delay();
+	
 	
 	s=s+1;
 	ss=ss+200;
 	if(v>50)
-	v=v-.8;
+	v=v-0.8;
 	yellow();
 	glPopMatrix();
 	}
@@ -449,15 +510,15 @@ void next()
 	
 	
 //***************FOR BOTTOM TO TOP******************
-	glPushMatrix();
+         glPushMatrix();
 	glTranslatef(m,n,0);
 	
 	if(up==1 && n<480)
 	{
 	
 	n=n+1;
-	if(m>350)
-	m--;
+	if(m>500)
+	m=m-1;
 	red();
 	glPopMatrix();
 	
@@ -511,8 +572,7 @@ void mydisplay(){
 		glutStrokeCharacter(GLUT_STROKE_MONO_ROMAN,strc[i]);
 		glPopMatrix();
 		
-		
-    	//renderBitmapString(420,870,"Computer Science and Engineering");
+
     	
     	
     	glColor3f(254/255,255/255,165/255);
@@ -557,11 +617,12 @@ void mydisplay(){
 	glVertex2i(1000,75);
 	glVertex2i(0,75);
 	glEnd();
-	glColor3f(1,0,1);
+	glColor3f(0.90,0.74,0.035);
     	
 	
 	renderBitmapString(850,100,"Press up arrow");
-	renderBitmapString(0,37,"DEADLOCK EXAMPLE OF TWO PEOPLE SHARING SAME LADDER");
+	glColor3f(0.16,0.109,0.156);
+	renderBitmapString(360,37,"DEADLOCK EXAMPLE OF TWO PERSONS USING SAME STEPS");
 	if(spotdisp==1)
 	
 	//************************************************************************************/
@@ -614,28 +675,61 @@ void mydisplay(){
    	 	glBegin(GL_QUADS);
     	glColor3f(0,0.015,0.156);
     	glVertex2f(0,0);
-    	glVertex2f(0,1000);
+    	glVertex2f(1000,0);
     	glColor3f(0,0.305,0.572);
     	glVertex2f(1000,1000);
-    	glVertex2f(1000,0);
+    	glVertex2f(0,1000);
     	glEnd();
     	
-    	flowrect(460,900);
     	
-    	flowrect(460,800);
-    	flowrect(460,700);
-    	flowrect(460,600);
-    	flowrect(250,600);
-    	flowrect(660,600);
-    	flowrect(460,500);
-    	flowrect(460,400);
-    	glColor3f(1,0,0);
-    	renderBitmapString(475,870,"START");
-    	renderBitmapString(475,770,"INTRO");
-    	renderBitmapString(475,670,"ABOUT");
-    	renderBitmapString(465,570,"EXAMPLE");
-    	renderBitmapString(265,570,"HELP");
-   	 }
+    	
+    	flowrect(450,900,90,50);//start
+    	flowrect(450,800,90,50);//intro
+    	flowrect(450,700,90,50);//about
+    	flowrect(415,600,160,50);//simulationpage 
+    	flowrect(250,600,90,50);//help
+    	//flowrect(660,600,70,40);
+    	flowrect(425,500,140,160);
+    	flowrect(460,200,70,40);//exit 
+    	glColor3f(1,0.4,0);
+    	renderBitmapString(475,867,"START");
+    	renderBitmapString(475,767,"INTRO");
+    	renderBitmapString(475,667,"ABOUT");
+    	renderBitmapString(437,567,"SIMULATION PAGE");
+    	renderBitmapString(455,460,"SIMULATION");
+    	renderBitmapString18(458,405,"Process=Person");
+    	renderBitmapString18(458,370,"Resource=Steps");
+    	
+    	renderBitmapString(480,170,"EXIT");
+    	renderBitmapString(280,567,"HELP");
+    	
+    	//lines for flowchart
+    	glLineWidth(3.0);
+    	line(495,850,495,800);
+    	line(495,750,495,700);
+    	line(495,650,495,600);
+    	line(495,550,495,500);
+    	line(340,575,415,575);
+    	line(295,550,295,180);
+    	line(295,180,460,180);
+    	line(575,575,695,575);
+    	line(695,575,695,180);
+    	line(695,180,530,180);
+    	//line(695,180,695,180);
+    	
+    	//triangle for arrows in flowchart
+    	arrow(495,800,0);
+    	arrow(495,700,0);
+    	arrow(495,600,0);
+    	arrow(340,575,1 );
+    	arrow(495,500,0);
+    	arrow(460,180,2); //exit left side
+    	arrow(530,180,1); //exit right side
+    	
+    	
+  
+    	
+	}
    	 
    	 
    	 if(f==5)
@@ -682,7 +776,10 @@ void mydisplay(){
    	glutSwapBuffers();
 	glutPostRedisplay();
 	glFlush();
-    	}
+    	
+}
+
+    	
     	
 
 
@@ -713,6 +810,19 @@ int main(int argc,char**argv)
 	glutMainLoop();
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
